99b279f0e735dc5f93c6ed2b666c846625678caf
willcapellaro <will@capellaro.com>
2026-04-01 08:23:48 -0500

    push new site

---
+++ node_modules/@mui/material/version/index.js
@@ -0,0 +1,12 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.version = exports.prerelease = exports.patch = exports.minor = exports.major = exports.default = void 0;
+const version = exports.version = "7.3.8";
+const major = exports.major = Number("7");
+const minor = exports.minor = Number("3");
+const patch = exports.patch = Number("8");
+const prerelease = exports.prerelease = undefined;
+var _default = exports.default = version;
